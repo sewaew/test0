770777266ce5ffd108cdaf3b34e20f5ce4bc5ceb
Evegenii Sevastjanov <sewaew@mail.ru>
2016-04-20 10:03:22 +0600

    first commit

---
+++ primer.py
@@ -0,0 +1,35 @@
+#пример
+#т.к. потребуется читать аргумент командной сроки
+import sys
+
+Zero = ["  ***  ", " *   * ", "*     *", "*     *", "*     *", " *   * ", "  ***  "]
+One = [" * ", "** ", " * ", " * ", " * ", " * ", "***"]
+Two = [" *** ", "*   *", "*   *", "   * ", "  *  ", " *   ", "*****"]
+Three = [" *** ", "*   *","    *", "   * ", "    *", "*   *", " *** "]
+Four = ["   * ", " * * ", "*  * ", "*****", "   * ", "   * ", "   * "]
+Five = ["******", "*    *", "*     "," **** ", "     *", "*    *", " **** "]
+Six =  [" **** ", "*    *", "*     ","***** ", "*    *", "*    *", " **** "]
+Seven = ["*****", "*   *", "    *", "   * ", "  *  ", " *   ", " *   "]
+Eight = [" **** ", "*    *", "*    *"," **** ", "*    *", "*    *", " **** "]
+Nine = [" **** ", "*    *", "*    *"," **** ", "     *", "     *", " **** "]
+
+Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
+#print (Digits)
+
+try:
+	digits = sys.argv[1]
+	row = 0
+	while row < 7:
+		line = ""
+		column = 0
+		while column < len(digits):
+			number = int(digits[column])
+			digit = Digits[number]
+			line += digit[row] + " "
+			column += 1
+		print(line)
+		row += 1
+except IndexError:
+	print("usage: bigdigits.py <number>")
+except ValueError as err:
+	print(err, "in", digits)
